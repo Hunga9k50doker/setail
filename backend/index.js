22dda8a70cf27521c525ddf8a38dde6a4a886ff3
Hunga9k50doker <19520101@gm.uit.edu.vn>
2023-04-09 22:56:30 +0700

    update cors

--- backend/index.js
+++ backend/index.js
@@ -11,14 +11,18 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://setail.onrender.com/", "http://localhost:3000"],
+  })
+);
 
 app.use("/api/cards", cardRoutes);
 app.use("/api/tours", tourRoutes);
 app.use("/api/user", userRoutes);
 
 const CONNECT_URL = process.env.CONNECT_URL;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 mongoose
   .connect(CONNECT_URL, { dbName: `setail`, useNewUrlParser: true, useUnifiedTopology: true })
