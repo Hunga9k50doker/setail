79307676fd2abdbbd3d7cb463c1206c3111cdf45
Hunga9k50doker <19520101@gm.uit.edu.vn>
2023-04-09 22:07:48 +0700

    update npm

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
 import cardRoutes from "./routes/cards.js";
 import tourRoutes from "./routes/tours.js";
 import userRoutes from "./routes/users.js";
 
+dotenv.config();
 const app = express();
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
@@ -17,7 +17,7 @@ app.use("/api/cards", cardRoutes);
 app.use("/api/tours", tourRoutes);
 app.use("/api/user", userRoutes);
 
-const CONNECT_URL = "mongodb+srv://nguyenhung:A9k50doker@cluster0.bun4q.mongodb.net/?retryWrites=true&w=majority";
+const CONNECT_URL = process.env.CONNECT_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose
