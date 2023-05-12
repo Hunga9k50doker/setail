eb680afccbdf0a025d9511319ddc27b01de16f17
Nguyễn Văn Hùng <66461092+Hunga9k50doker@users.noreply.github.com>
2023-05-12 22:56:16 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    origin: true,
+    origin: false,
   })
 );
 
